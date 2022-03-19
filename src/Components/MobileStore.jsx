8e6863e2c93c4235fc7664823cedb742cf415273
machineawin <machineawin@gmail.com>
2022-03-20 10:06:24 +1100

    Add css to Mobile Phone

--- src/Components/MobileStore.jsx
+++ src/Components/MobileStore.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import MobilePhones from './MobilePhones'
 
 const MobileStore = () => {
   return (
     <div className='container'>
         <Header title='Mobile Store' />
+        <MobilePhones />
     </div>
   )
 }
